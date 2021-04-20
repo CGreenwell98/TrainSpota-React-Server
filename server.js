ced538b559c127c3889920f21391286a23b274c4
CGreenwell98 <christophergreenwell7@gmail.com>
2021-04-20 15:05:36 +0100

    build folder added, path code added, ready for deploy

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use("/map", mapRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "build")));
 
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 }
