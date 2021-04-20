462ce2f8147a266821f49aba8324c9c7df80911f
CGreenwell98 <christophergreenwell7@gmail.com>
2021-04-20 14:41:46 +0100

    build folder added, path code added, ready for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("dotenv").config();
 
@@ -15,11 +16,19 @@ app.use(express.static("public"));
 app.use("/user", userRoutes);
 app.use("/map", mapRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 // const MONGO_DB_URL = ""
 // mongoose.connect(MONGO_DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 // .then(() => console.log("MongoDB connected..."))
 // .catch(err => console.log(err))
 // mongoose.set("useFindAndModify", false);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
